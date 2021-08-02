dc6b0e963697c373662bbbe0e17aaff8edd38c6d
rach-zhang <88282992+rach-zhang@users.noreply.github.com>
2021-08-02 02:52:05 -0400

    Uploaded lesson 2 worksheet.

---
+++ python/unit-2/lesson_2.py
@@ -0,0 +1,39 @@
+Unit 2 Lesson 2 Worksheet
+
+Rachel Zhang
+
+1. What is Python and why is it useful?
+
+Python is a general purpose, interpretive, interactive, object-oriented, and high-level programming language. It is useful for data analytics because it can work with very large datasets, perform complex operations, and Python code can be saved and re-run.
+
+2. Are the following variable names allowed in Python?
+
+a. 1_message: Yes
+b. Greeting_message: Yes
+c. Message_1: Yes
+d. First name: No
+e. Full_name: Yes
+
+3. Create a variable that holds the string "hello there!"
+
+message = "hello there!"
+print(message)
+
+4. Create a variable for first name, last name, and an email extension. Concatenating all three together to form an email address.
+
+first_name = "rachel"
+last_name = "zhang"
+email_extension = "@gmail.com"
+email_address = first_name + last_name + email_extension
+print(email_address)
+
+5. Store someone you know name in a variable called name. Print their name in lower and uppercase using a method.
+name = 'Henrietta'
+
+print(name.lower())
+print(name.upper())
+
+6. Using a variable, ask your friend if they want to hang out on the 15th of the month.
+
+date = 15
+print("Hey Max, do you want to hang out on the " + str(date) + "th of this month?")
