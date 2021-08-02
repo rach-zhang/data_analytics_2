0eb0fbabbedccef7fe6f63241712dfe6b1339089
rach-zhang <88282992+rach-zhang@users.noreply.github.com>
2021-08-02 02:25:49 -0400

    Fixed issue involving weapon input case.

--- python/unit-2/rock-paper-scissors.py
+++ python/unit-2/rock-paper-scissors.py
@@ -7,11 +7,11 @@ while another_round.title() == "Yes":
   player_1_weapon = input("Player 1: What is your weapon?")
   player_2_weapon = input("Player 2: What is your weapon?")
 
-  if (player_1_weapon == 'rock' and player_2_weapon == 'scissors') or \
-  (player_1_weapon == 'scissors' and player_2_weapon == 'paper') or \
-  (player_1_weapon == 'paper' and player_2_weapon == 'rock'):
+  if (player_1_weapon.lower() == 'rock' and player_2_weapon.lower() == 'scissors') or \
+  (player_1_weapon.lower() == 'scissors' and player_2_weapon.lower() == 'paper') or \
+  (player_1_weapon.lower() == 'paper' and player_2_weapon.lower() == 'rock'):
     result("Player 1", player_1_weapon, player_2_weapon)
-  elif player_1_weapon == player_2_weapon:
+  elif player_1_weapon.lower() == player_2_weapon.lower():
     print ("Draw.")
   else:
     result("Player 2", player_2_weapon, player_1_weapon)
